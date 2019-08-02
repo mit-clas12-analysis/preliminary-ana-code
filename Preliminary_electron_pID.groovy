8beb2e0cab8b7b02a92cc6019f0dcc91aff22ee6
Sangbaek Lee <sangbaek@jlab.org>
2019-08-02 05:22:41 -0400

    correcting typos, errors

--- Preliminary_electron_pID.groovy
+++ Preliminary_electron_pID.groovy
@@ -25,25 +25,33 @@ float EB = 10.6f
 if(run>6607) EB=10.2f
 // H1F H_elec_mom[6];
 // H2F H_elec_vz_mom[6], H_elec_theta_mom[6], H_elec_theta_phi[6];
-def H_elec_mom =(0..<6).collect{
+H_elec_mom =(0..5).collect{
 	def h1 = new H1F("H_elec_mom_S"+(it+1), "H_elec_mom_S"+(it+1),100, 0, EB);
 	return h1
 }
 
-def H_elec_vz_mom =(0..<6).collect{
+H_elec_vz_mom =(0..<6).collect{
 	def h1 = new H2F("H_elec_vz_mom_S"+(it+1), "H_elec_vz_mom_S"+(it+1),100,0,EB,100,-25,25);
 	return h1
 }
 
-def H_elec_theta_mom =(0..<6).collect{
+H_elec_theta_mom =(0..<6).collect{
 	def h1 = new H2F("H_elec_theta_mom_S"+(it+1), "H_elec_theta_mom_S"+(it+1),100,0,EB,100,0,40);
 	return h1
 }
 
-def H_elec_theta_phi =(0..<6).collect{
+H_elec_phi_mom = (0..<6).collect{
+	def h1 = new H2F("H_elec_phi_mom_S"+(it+1), "H_elec_phi_mom_S"+(it+1),100,0,EB,100,-180,180);
+	return h1
+}
+
+H_elec_theta_phi =(0..<6).collect{
 	def h1 = new H2F("H_elec_theta_phi_S"+(it+1), "H_elec_theta_phi_S"+(it+1),100,-180,180,100,0,40);
 	return h1
 }
+
+//H_elec_mom=new H1F("H_elec_mom", "H_elec_mom_S",100, 0, EB);
+
 
 int e_index, e_sect
 float e_mom, e_theta, e_phi, e_vx, e_vy, e_vz
@@ -66,28 +74,33 @@ for (arg in args){
 	out.addDataSet(H_elec_mom[it])
 	out.addDataSet(H_elec_vz_mom[it])
 	out.addDataSet(H_elec_theta_mom[it])
+	out.addDataSet(H_elec_phi_mom[it])
 	out.addDataSet(H_elec_theta_phi[it])
 }
 
+//out.addDataSet(H_elec_mom)
 out.writeFile('electron_pID_'+run+'.hipo')
 
 public void processEvent(DataEvent event) {
-	if(!event.hasBank("REC::Particle")) return;
+	if(!event.hasBank("REC::Particle")) return 
 	DataBank partBank = event.getBank("REC::Particle");
 	e_index=-1
-	if(!hasElectron(partBank)) return;
+	if (!hasElectron(partBank)) return
 	if (e_index>-1){
 		makeElectron(partBank)
-		fillHists();
+		fillHists()
+		//H_elec_mom.fill(e_mom)
 	}
 	else return;
 }
 
+
 public void fillHists(){
-	H_elec_mom[e_sect].fill(e_mom);
-	H_elec_vz_mom[e_sect].fill(e_mom,e_vz);
-	H_elec_theta_mom[e_sect].fill(e_mom,e_theta)
-	H_elec_theta_phi[e_sect].fill(e_phi,e_theta);
+	H_elec_mom[e_sect-1].fill(e_mom);
+	H_elec_vz_mom[e_sect-1].fill(e_mom,e_vz);
+	H_elec_theta_mom[e_sect-1].fill(e_mom,e_theta)
+	H_elec_phi_mom[e_sect-1].fill(e_mom,e_phi)
+	H_elec_theta_phi[e_sect-1].fill(e_phi,e_theta);
 }
 
 public void makeElectron(DataBank recPart){
@@ -100,12 +113,14 @@ public void makeElectron(DataBank recPart){
 		e_vx = recPart.getFloat("vx",ei)
 		e_vy = recPart.getFloat("vy",ei)
 		e_phi = (float)Math.toDegrees(Math.atan2(py,px))
-		e_phi+=20;
-		if(phi<0) e_phi+=180;
-		e_sect = (int)e_phi/60;
+		if(e_phi<0) e_phi+=360;
+		e_phi=360-e_phi;
+		e_phi=e_phi-150;
+		if (e_phi<0) e_phi+=360;
+		e_sect = (int) Math.ceil(e_phi/60);
 		Ve = new LorentzVector(px,py,pz,e_mom)
-		e_phi = (float) Math.toDegrees(Math.toDegrees(VE.phi()))
-		e_theta = (float) Math.toDegrees(Math.toDegrees(VE.theta()))
+		e_phi = (float) Math.toDegrees(Ve.phi())
+		e_theta = (float) Math.toDegrees(Ve.theta())
 }
 
 public boolean hasElectron(DataBank recPart){
@@ -121,7 +136,7 @@ public boolean hasElectron(DataBank recPart){
 
 public boolean isElectron(DataBank recPart, int p){
 	if (ele_default_PID_cut(recPart,p)&& ele_charge_cut(recPart,p) && ele_kine_cut(recPart,p)&& inDC(recPart,p)){
-		System.out.println("Electron Found!")
+		//System.out.println("Electron Found!")
 		e_index=p
 		return true
 	}
@@ -129,7 +144,7 @@ public boolean isElectron(DataBank recPart, int p){
 }
 
 public boolean inDC(DataBank recPart, int p){
-	int status = bank.getShort("status", p);
+	int status = recPart.getShort("status", p);
 	if (status<0) status = -status;
 	boolean inDC = (status>=2000 && status<4000);
 }
@@ -141,7 +156,7 @@ public boolean ele_default_PID_cut(DataBank recPart, int p){
 }
 
 public boolean ele_charge_cut(DataBank recPart, int p){
-  if(recPart.getInt("charge",p)==11) return true;
+  if(recPart.getInt("charge",p)==-1) return true;
   else return false;
 }
 
@@ -153,8 +168,6 @@ public boolean ele_kine_cut(DataBank recPart, int p){
 	float mom = (float)Math.sqrt(px*px+py*py+pz*pz);
 	float theta = (float)Math.toDegrees(Math.acos(pz/mom));
 	float phi = (float)Math.toDegrees(Math.atan2(py,px));
-	phi+=20;
-	if(phi<0)phi+=180;
 	if(mom>1.75 && theta>7 && Math.abs(vz)<15 && theta>17*(1-mom/7) ){
 		return true;
 	}
